55b7756f17f88ec14527287c5fc6dc8c16e330bd
chablau234 <165678301+Daniel8292@users.noreply.github.com>
2026-02-23 08:21:15 -0300

    pode cre

---
+++ teobaldo.js
@@ -0,0 +1,8 @@
+//em JavaScript tem tres tipos de dados o booleano, numérico e a string
+//o numérico é chamado de number
+//os dados são armazenados em constantes e variaveis
+const meuNumero = 12
+const primeiroNumero = 1
+const  segundoNumero = 3
+const operacao = primeiroNumero + segundoNumero
+console.log (operacao)
