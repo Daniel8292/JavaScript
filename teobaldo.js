6f2526b2d5d339b3fa1933f98ff7c6f5e69b4f3c
chablau234 <165678301+Daniel8292@users.noreply.github.com>
2026-02-24 08:20:52 -0300

    plau

--- teobaldo.js
+++ teobaldo.js
@@ -1,8 +1,20 @@
 //em JavaScript tem tres tipos de dados o booleano, numérico e a string
 //o numérico é chamado de number
 //os dados são armazenados em constantes e variaveis
+
 const meuNumero = 12
 const primeiroNumero = 1
 const  segundoNumero = 3
 const operacao = primeiroNumero + segundoNumero
 console.log (operacao)
+
+//pontos flutuantes são numeros com vírgula
+
+const numeroDecimal = 6.9
+const numeroFlutuanteSemZero = .7
+const novaOperacao = numeroDecimal/numeroFlutuanteSemZero
+const colegio = "mario braga"
+console.log (colegio*primeiroNumero)
+
+//o NaN(not at number) aparece quando voce tenta fazer uma operaco matematica com uma string
+
